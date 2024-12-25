0d9f1142e41302ed7dad7af5978eb4b91a21a083
Thinh <phuocthinh1025@gmail.com>
2024-12-26 02:19:59 +0700

    Fix Cart Item Error

--- src/main/java/com/example/canteen/service/CartItemService.java
+++ src/main/java/com/example/canteen/service/CartItemService.java
@@ -8,11 +8,9 @@ import com.example.canteen.exception.AppException;
 import com.example.canteen.enums.ErrorCode;
 import com.example.canteen.repository.CartItemRepository;
 import com.example.canteen.repository.CartRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
@@ -24,7 +22,6 @@ public class CartItemService {
     private final ProductService productService;
     private final CartService cartService;
 
-    private static final Logger logger = LoggerFactory.getLogger(CartItemService.class);
 
     public void addItemtoCart(Long cartId, Long productId, int quantity){
         //1. Get the cart
@@ -54,26 +51,15 @@ public class CartItemService {
         cartItemRepository.save(cartItem);
         cartRepository.save(cart);
     }
-
-    @Transactional
-    public void removeItemFromCart(Long cartId, Long productId){
-        logger.info("Attempting to remove product with ID {} from cart with ID {}", productId, cartId);
-
+    public void removeItemFromCart(Long cartId, Long itemId){
         Cart cart = cartService.getCart(cartId);
         CartItem itemToRemove = cart.getItems()
                 .stream()
-                .filter(item -> item.getProduct().getId().equals(productId))
+                .filter(item -> item.getId().equals(itemId))
                 .findFirst()
-                .orElseThrow(() -> {
-                    logger.error("Product with ID {} not found in cart with ID {}", productId, cartId);
-                    return new AppException(ErrorCode.PRODUCT_NOT_FOUND);
-                });
-
+                .orElseThrow(() -> new AppException(ErrorCode.PRODUCT_NOT_FOUND));
         cart.removeItem(itemToRemove);
-        cartItemRepository.delete(itemToRemove);
         cartRepository.save(cart);
-
-        logger.info("Product with ID {} successfully removed from cart with ID {}", productId, cartId);
     }
 
     public void updateItemQuantity(Long cartId, Long productId, int quantity){
